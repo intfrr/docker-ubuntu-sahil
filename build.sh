1eb18cb0f37174110704ca69b4a1275da76b157b
John W <kaldt.slange@gmail.com>
2020-01-25 15:54:00 -0600

    2001151553 Push docke image

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build --rm -f Dockerfile -t ubuntu:sahil .
+#!/bin/zsh
+#docker build --rm -f Dockerfile -t ubuntu:sahil .
+#docker build --rm -f Dockerfile -t intfrr/taiga-dev:latest .
+docker build --rm -f Dockerfile -t intfrr/taiga-dev:1.2 .
--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-docker run --rm -it ubuntu:sahil 
+#!/bin/zsh
+#docker run --rm -it ubuntu:sahil
+#docker run --rm -it intfrr/taiga-dev:latest
+#docker run --rm -it intfrr/taiga-dev:1.1
+
+
+#docker run -it -p 8000:8000 -v "/Users/dragon/DOCS/PERS/ACTION/RANGU/CALENDAR":"/home/devuser/" --name 'intfrr-taiga-dev_v1.1' intfrr/taiga-dev:1.1
+#docker run -d -p 8000:8000 -v "/Users/dragon/DOCS/PERS/ACTION/RANGU/CALENDAR":"/home/devuser/" --name 'intfrr-taiga-dev_v1.1.1' intfrr/taiga-dev:1.1
+docker start intfrr-taiga-dev_v1.2 && docker exec -it intfrr-taiga-dev_v1.2 /bin/zsh
